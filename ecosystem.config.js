c5d6ca1db4a6f7f1ed239f43311d7efbf4fab858
Youssef-Durgham <yusifdhrgamtrt@gmail.com>
2025-03-03 10:30:02 +0200

    fix login issues

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       cwd: "./", // Ensure this points to your project root
       env: {
         NODE_ENV: "production",
-        PORT: 9090, // Specify the port if needed
+        PORT: 3000, // Specify the port if needed
       },
       error_file: "logs/err.log",
       out_file: "logs/out.log",
